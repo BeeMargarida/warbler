c3f0713ee76401dee563fd938c0cbfa6f9aaa354
BeeMargarida <up201505505@gcloud.fe.up.pt>
2019-02-06 23:19:10 +0000

    warbler server done and created react app for frontend

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -4,7 +4,10 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
+const db = require("./models");
 const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
+const { loginRequired, ensureCorrectUser } = require("./middleware/auth");
 
 const PORT = 8081;
 
@@ -12,8 +15,24 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users/:id/messages", loginRequired, ensureCorrectUser, messagesRoutes);
+app.use("/api/messages", loginRequired, async function (req, res, next) {
 
-app.use(function(req, res, next) {
+    try {
+
+        let messages = await db.Message.find()
+            .sort({ createdAt: "desc" })
+            .populate("user", { username: true, profileImageUrl: true });
+
+        return res.status(200).json(messages);
+
+    } catch (err) {
+        return next(err);
+    }
+
+})
+
+app.use(function (req, res, next) {
     let err = new Error("Not Found");
     err.status = 404;
     next(err);
@@ -21,6 +40,6 @@ app.use(function(req, res, next) {
 
 app.use(errorHandler);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`Server is starting on port ${PORT}`);
 });
--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/warbler", {
-    keepAlive: true,
-    useMongoClient: true
+mongoose.connect("mongodb://localhost:27018/warbler", {
+    keepAlive: true
 });
 
 module.exports.User = require("./user");
+module.exports.Message = require("./message");
